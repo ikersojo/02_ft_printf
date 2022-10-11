96363a49f7c503ddc15ded95a62ce1825db21392
Iker Sojo <ikersojo@hotmail.com>
2022-10-11 18:09:28 +0200

    WIP

--- main.c
+++ main.c
@@ -3,25 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
+/*   By: isojo-go <isojo-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/05 22:51:57 by isojo-go          #+#    #+#             */
-/*   Updated: 2022/10/06 11:38:03 by isojo-go         ###   ########.fr       */
+/*   Created: 2022/10/11 17:57:29 by isojo-go          #+#    #+#             */
+/*   Updated: 2022/10/11 18:07:04 by isojo-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
 #include "./libft/inc/libft.h"
+#include "ft_printf.h"
 #include <stdio.h>
 
 int	main(void)
 {
-	int	i;
-	int	j;
 	int	num;
+	int i;
+	int	j;
 
-	num = 0;
-	i = ft_printf("%d\n", num);
-	j = printf("%d\n", num);
-	printf("%d - %d", i, j);
+	num = 2147483647;
+	
+	i = printf(" c_printf: %d\n", num);
+	j = ft_printf("ft_printf: %d\n", num);
+	printf(" c: %d\nft: %d\n", i, j);
+	return (0);
 }
